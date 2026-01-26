be486430a2d6fe183b0c2cb115c37ba4a2bad88a
Ital023 <contadoitalo911@gmail.com>
2026-01-26 12:57:42 -0300

    init: initial files

---
+++ src/test/java/io/github/Ital023/EcommerceItalo/EcommerceItaloApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.EcommerceItalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceItaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
